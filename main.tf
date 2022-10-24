c3a139ff85afde1d9cd4e2d76131c0d078bb7f64
Fabrizzio Casamento <casamento@gmail.com>
2022-10-24 01:29:33 -0300

    correção de variaveis

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ output "jenkins_ip" {
   value       = digitalocean_droplet.jenkins.ipv4_address
 }
 
-resource "local_file" "foo" {
+resource "local_file" "kube_config" {
     content  = digitalocean_kubernetes_cluster.k8s.kube_config.0.raw_config
     filename = "kube_config.yaml"
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,3 @@
-do_token     = "<alterar>"
-ssh_key_name = "casamento"
+do_token     = "dop_v1_d0b1900a97cf7a73be4a41296e56fd46e75738a238dc07c79a47024800a6fea9"
+ssh_key_name = "ssh_key"
 region       = "nyc1"
